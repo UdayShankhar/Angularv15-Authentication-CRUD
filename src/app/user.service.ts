f3de6ac5271970cc6fec6393f3e85cd94dce4594
PickMyAd <pma-mac-01@PickMyAds-MacBook-Pro.local>
2023-11-29 12:09:23 +0530

    CSS and service file changes

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -20,22 +20,22 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/users/allUsers`);
+    return this.http.get(`${this.apiUrl}/users/allUsers`);
   }
 
   getUserById(userId: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/users/${userId}`);
+    return this.http.get(`${this.apiUrl}/users/${userId}`);
   }
 
   addUser(user: User): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/users/addUser`, user);
+    return this.http.post(`${this.apiUrl}/users/addUser`, user);
   }
 
   updateUser(userId: string, user: User): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/users/edit/${userId}`, user);
+    return this.http.put(`${this.apiUrl}/users/edit/${userId}`, user);
   }
 
   deleteUser(userId: string | unknown): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/users/delete/${userId}`);
+    return this.http.delete(`${this.apiUrl}/users/delete/${userId}`);
   }
 }
